4bc3943ffe8156e4288d7faf66d97aa616ef6dc1
aMontoya224 <jmontoyaan@unsa.edu.pe>
2024-06-11 00:43:52 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [device, setDevice] = useState(false);
+  const [ledGreenStatus, setLedGreenStatus] = useState(false);
+  const [ledRedStatus, setLedRedStatus] = useState(false);
+  const [potValue, setPotValue] = useState(0);
+  const ESP32_IP = 'http://192.168.1.38';
+
+  const handleRoot = () => {
+    axios.get(`${ESP32_IP}/`)
+      .then(() => {
+        setLedGreenStatus(false);
+        setLedRedStatus(false);
+        setDevice(true);
+      })
+      .catch(error => {
+        setDevice(false);
+        console.error('Error:', error);
+      });
+  };
+
+  const handleLed = (led, e) => {
+    let status = 'OFF';
+    if(e.target.checked){
+      status = 'ON';
+    }
+    else{
+      status = 'OFF';
+    }
+    axios.get(`${ESP32_IP}/LED/${led}/${status}`)
+      .then(() => {
+        if(led === 'GREEN'){
+          setLedGreenStatus(status === 'ON' ? true : false);
+        }
+        else{
+          setLedRedStatus(status === 'ON' ? true : false);
+        }
+        setDevice(true);
+      })
+      .catch(error => {
+        setDevice(false);
+        console.error('Error:', error);
+      });
+  };
+
+  const readSensor = () => {
+    axios.get(`${ESP32_IP}/POT`)
+      .then(response => {
+        setPotValue(response.data);
+        setDevice(true);
+      })
+      .catch(error => {
+        setDevice(false);
+        console.error('Error:', error);
+      });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(readSensor, 500);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>hola</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div>
+          <b>Device</b>
+          <div>
+            <div className={device ? 'success' : 'danger'}></div>
+            <p>{device ? 'online' : 'offline'}</p>
+          </div>
+        </div>
+        <button onClick={handleRoot}>ROOT</button>
+        <div></div>
       </header>
+      <main>
+        <div className='widget'>
+          <p>El LED VERDE está: {ledGreenStatus}</p>
+          <div class="toggle">
+            <input type="checkbox" id="green" onClick={(e) => handleLed('GREEN', e)} checked={ledGreenStatus}/>
+            <label for="green"></label>
+          </div>
+        </div>
+        <div className='widget'>
+          <p>El LED ROJO está: {ledRedStatus}</p>
+          <div class="toggle">
+            <input type="checkbox" id="red" onClick={(e) => handleLed('RED', e)} checked={ledRedStatus}/>
+            <label for="red"></label>
+          </div>
+        </div>
+        <div className='widget'>
+          <p>Valor del Potenciómetro:</p>
+          <h1>{potValue}</h1>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
